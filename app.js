617aa86a275a81127ea80cc84a3a2be6a6548c34
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-02-23 14:42:18 -0300

    Added steps to follow

---
+++ app.js
@@ -0,0 +1,12 @@
+// Steps
+
+// 1. Get the canvas and context
+// 2. Define the vertices of a shape
+// 3. Create a buffer to store the vertices
+// 4. Bind the buffer to the webgl context
+// 5. Create the shaders
+// 6. Create the shader program
+// 7. Bind the vertex buffer to the shader program and enable attributes, uniforms, etc
+// 8. Draw the shape
+
+// ✨🍰✨
